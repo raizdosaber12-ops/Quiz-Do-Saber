380051523b3d2224735f63b57173f0d8fd40d422
raizdosaber12-ops <raizdosaber12@gmail.com>
2026-07-01 12:08:20 +0100

    Add files via upload
    empreendedorismo

---
+++ javascript/empreendedorismo.js
@@ -0,0 +1,77 @@
+var perguntasEmpreendedorismo = {
+  nivel1: [
+    // VERDADEIRO/FALSO
+    { tipo: 'vf', pergunta: 'Os tipos de oportunidades negócio são: Identificão de necessidades;Observação de dificuldades;Observação de tendências;Derivação da ocupação actual;Procura de novas aplicações;Exploração de passatempos;Moda;Imitação do sucesso de outras pessoas.', resposta: true },
+
+    { tipo: 'vf', pergunta: 'Disney tem três característica que são:Crítico,Sonhador,Realista.', resposta: true },
+
+    { tipo: 'vf', pergunta: 'Plano de negócio um documento que serve para elaborar directrizes para o negócio.', resposta: true },
+
+    { tipo: 'vf', pergunta:'Um plano de negócio de ser:Simples,Objectivo,Atrair recursos financeiros;Concreto e Completo.', resposta: true },
+
+    { tipo: 'vf',pergunta:' Análise do mercado não e fundamental para conhecer o mercado onde a empresa vai actuar.', resposta: false },
+
+    { tipo: 'vf', pergunta: 'Análise da procura,Potênciais clientes,análise da oferta e Análise da concorrência não são importantes para analisar o mercado.', resposta: false },
+
+    { tipo: 'vf', pergunta: 'As variáveis que mais valor produzem para os clientes e que melhor diferenciam a empresa dos seus concorrentes,Devem ser claramente identificadas,Devem apostar e afectar grande parte dos recursos financeiros,materiais e humanos nelas.',resposta: true },
+
+    { tipo: 'vf', pergunta: 'Oportunidades e ameaças,Ponto fortes e fracos fazem parte da Visão e Missão.', resposta: false },
+
+    { tipo: 'vf', pergunta: 'A Análise FOFA também designada de analise SWOT e feita através da utilização da Matriz FOFA.', resposta: true },
+
+    { tipo: 'vf', pergunta: 'Oportunidades e ameaças,Pontos fortes e fracos fazem parte dos objectivos da analise fofa.', resposta: true },
+
+    // MÚLTIPLA ESCOLHA
+    { tipo: 'mc', pergunta: 'Qual das seguintes opções representa os quatro ps de Marketing Mix?', opcoes: ['Produto,Preço ,Distribuição e Promoção', 'Empresa e Pessoa', 'Distribuição, Marketing.'], resposta: 0 },
+
+    { tipo: 'mc', pergunta: 'Os quatro ps que Kotler identificou são:', opcoes: ['Distribuição,Promoção e pessoa', 'Pessoas,Processos,programa e Performance', 'Programa,Promoção,Distribuição e Preço.' ], resposta: 1 },
+
+    { tipo: 'mc', pergunta: 'O Plano de Organização de Recursos Humanos tem como objectivo a apresentação da estrutura organizativa e planificação dos recursos humanos e consiste:', opcoes: [' Apresentação da equipe de trabalho ,número de trabalhadores,funções e estrutura,Organograma,Capacidades necessárias e perfil desejado', 'Explicar a competência externa necessária,Anexar os currículos vitae dos executivos principais e mostrar que estes são capazes de superar desafios.' ], resposta: 1 },
+
+    { tipo: 'mc', pergunta: 'A probabilidade do sucesso de um negócio depende dos seguintes factores:', opcoes: ['Plano de investimento,Plano de financiamento,Identificadores de gestão,Análise de viabilidade económico financeiro', 'Preço de venda,Quantidade de venda,Alterações na Estrutura de Custos,Alteração de taxa de juro,taxa de inflação.' ], resposta: 0 },
+ 
+   
+  ],
+
+  nivel2: [
+    // DISSERTIVA (Correta/Não correta)
+    { tipo: 'dissertiva', pergunta: 'Para finalizar o plano de negócios devemos incluir todos os documentos que considerase relevante e que possam dar suporte e dar credibilidade ao teu projecto.', resposta: true },
+
+    
+
+    { tipo: 'dissertiva', pergunta: 'Os aspectos a reter ao  fazer o aprovisionamento são: Determinação da qualidade de bens e serviços a adquirir, preço e data de aquisição.', resposta: true },
+
+    { tipo: 'dissertiva', pergunta: 'As fontes de financimento são:As poupanças pessoais do empreendedor,Os empréstimos bancários,Os empréstimos de familiares e amigos.', resposta: true },
+
+    { tipo: 'dissertiva', pergunta: 'As informações que podemos obter no documento chamado fixa de armazém são: Conhecer o stock existente de armazém, evitar roptura ou excessos de um determinado produto, calcular o custo de um determinado produto e permitir uma leitura fácil da dados.', resposta: true},
+
+    { tipo: 'dissertiva', pergunta: 'A poupança são as fontes externas de financiamento mais comum para determinar o risco de empréstimo.', resposta: false },
+
+    { tipo: 'dissertiva', pergunta: 'O plano financeiro não é uma actividade essencial na gestão e nas financas da empresa, pois afecta todo o funcionamento da empresa.', resposta: true },
+
+     { tipo: 'dissertiva', pergunta: 'Um plano de negócios de conter os seguintes pontos:Sumário Executivo,Problema bem descrito,Solução facilmente entendida,Oportunidade de negócios de elevado potencial,Expectativa de retorno elevado.', resposta: true },
+
+  ],
+
+  nivel3: [
+  
+    // LIGAÇÃO DE COLUNAS
+   {
+  tipo: 'ligacao',
+  pergunta: 'Liga cada conceito à sua definição:',
+  colunaA: [
+    'Plano de negócios',
+    'Guiché',
+    'poupanca'
+  ],
+  colunaB: [
+    'Ferramenta de gestão fundamental',
+    'Serviço que simplifica e facilita o processo de constituição,alteração ou dissolução de empresas e actos afins.',
+    'é o valor excedente de um rendimento.'
+  ],
+  resposta: [0,1,2]
+},
+    
+   
+  ],
+};
